6da59991dba7b9efb22b41fb702185e8dfe9d8a4
ChaosBah <37620958+ChaosBah@users.noreply.github.com>
2018-10-24 23:57:53 +0300

    Update ThingService.java

--- src/main/java/com/shilko/ru/witcher/service/ThingService.java
+++ src/main/java/com/shilko/ru/witcher/service/ThingService.java
@@ -1,4 +1,18 @@
 package com.shilko.ru.witcher.service;
 
 public interface ThingService {
+  
+  //adding:
+  //arg -> ???
+  void addNewThing(Thing thing);
+  
+  //edditing:
+  void editThing(Long whichThing, Thing thisThing);
+  
+  //getters:
+  Thing getThingById(Long id);
+  Thign getThingByName(String thingName);
+  //some list or map here:
+  //Thing getAllThings();
+  
 }
