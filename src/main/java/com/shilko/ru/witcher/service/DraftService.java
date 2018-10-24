410c815baeb48f5c2de1401353065f2dd16ebf38
ChaosBah <37620958+ChaosBah@users.noreply.github.com>
2018-10-25 00:21:49 +0300

    Update DraftService.java

--- src/main/java/com/shilko/ru/witcher/service/DraftService.java
+++ src/main/java/com/shilko/ru/witcher/service/DraftService.java
@@ -1,4 +1,17 @@
 package com.shilko.ru.witcher.service;
 
 public interface DraftService {
+  
+  //What does this method return???
+  // draft(Component whichComponent, Thing whichThing);
+  
+  //check on draft:
+  boolean isComponentByDraft(Component component);
+  boolean isThingByDraft(Thing thing);  
+  
+  //add these two methods:
+  
+  void saveDraftResult();
+  
+  void mailtoDraftResult();
 }
