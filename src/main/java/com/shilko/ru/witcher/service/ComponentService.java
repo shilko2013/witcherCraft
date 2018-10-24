49a249fcfd9aebe83733f6bfc601d7fc327aa683
ChaosBah <37620958+ChaosBah@users.noreply.github.com>
2018-10-25 00:03:48 +0300

    Update ComponentService.java

--- src/main/java/com/shilko/ru/witcher/service/ComponentService.java
+++ src/main/java/com/shilko/ru/witcher/service/ComponentService.java
@@ -2,5 +2,16 @@ package com.shilko.ru.witcher.service;
 
 public interface ComponentService {
 
+  //additing:
+  void addNewComponent(Component newComponent);
+  
+  //edditing:
+  void editComponent(Long whichComponent, Component thisComponent);
+  
+  //getters:
+  Component getComponentById(Long id);
+  Component getComponentByName(String name);
+  //here a list or map
+  //<Component> getAllComponents();
 
 }
