15761c8acba64fbc0789854119a16ba96f115211
ChaosBah <37620958+ChaosBah@users.noreply.github.com>
2018-10-25 00:09:33 +0300

    Update BankService.java

--- src/main/java/com/shilko/ru/witcher/service/BankService.java
+++ src/main/java/com/shilko/ru/witcher/service/BankService.java
@@ -1,4 +1,10 @@
 package com.shilko.ru.witcher.service;
 
 public interface BankService {
+  
+  void takeMoney(Long money);
+  
+  //what did you mean on this methon???
+  void doTranzaction();
+  
 }
