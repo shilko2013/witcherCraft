cd30fb21f7432fbf71f874b1aa472ecd518b6aa9
ChaosBah <37620958+ChaosBah@users.noreply.github.com>
2018-10-25 00:15:08 +0300

    Update AdminService.java

--- src/main/java/com/shilko/ru/witcher/service/AdminService.java
+++ src/main/java/com/shilko/ru/witcher/service/AdminService.java
@@ -2,12 +2,16 @@ package com.shilko.ru.witcher.service;
 
 public interface AdminService {
   
+  //Others can have got this ability too!!!
   void addNewPage();
   
+  //Are you sure that this must be here???
   void authorization();
   
+  //Other can have got this ability too!!!
   void editPageContent();
   
+  //Add this method!!!
   void confirmEditing();
   
 }
