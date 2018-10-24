45d18dcba68e6557e2081653d4bf7b395291126b
ChaosBah <37620958+ChaosBah@users.noreply.github.com>
2018-10-25 00:12:32 +0300

    Update AnalyticsService.java

--- src/main/java/com/shilko/ru/witcher/service/AnalyticsService.java
+++ src/main/java/com/shilko/ru/witcher/service/AnalyticsService.java
@@ -1,4 +1,13 @@
 package com.shilko.ru.witcher.service;
 
 public interface AnalyticsService {
+  
+  //What does this method do???
+  void loggingAnalitics();
+  
+  //Which type does this getter return???
+  void getAnalitics();
+  
+  void updateAndlitics();
+  
 }
