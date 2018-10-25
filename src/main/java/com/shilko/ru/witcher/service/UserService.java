6cca28430cb90482363f998661e8bbe0d3c82817
ChaosBah <37620958+ChaosBah@users.noreply.github.com>
2018-10-25 20:23:39 +0300

    Update UserService.java

--- src/main/java/com/shilko/ru/witcher/service/UserService.java
+++ src/main/java/com/shilko/ru/witcher/service/UserService.java
@@ -4,15 +4,11 @@ import com.shilko.ru.witcher.entity.Users;
 
 public interface UserService {
 
-    void addUser(Users users);
+    void addUser(Users newUser);
+    
+    void editUser(Users editedUser);
 
     Users findByLogin(String login);
 
     Users findByEmail(String email);
-    
-    void editUser(Users editedUser);
-    
-    void editPageContent(String edits);
-    
-    void addNewContent(String news);
 }
