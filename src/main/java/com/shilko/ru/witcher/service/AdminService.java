62b2730bb006e76f4892ba4656c7a5c5f17d0570
ChaosBah <37620958+ChaosBah@users.noreply.github.com>
2018-10-25 20:25:19 +0300

    Update AdminService.java

--- src/main/java/com/shilko/ru/witcher/service/AdminService.java
+++ src/main/java/com/shilko/ru/witcher/service/AdminService.java
@@ -2,7 +2,8 @@ package com.shilko.ru.witcher.service;
 
 public interface AdminService {
  
-  //Are you sure that this must be here???
-  void authorization();
-  
+ confirmEditedContent();
+ 
+ 
+ 
 }
