6a70cd0eaf44d6743bb5ed6da0ad5ac7c1b544ed
ChaosBah <37620958+ChaosBah@users.noreply.github.com>
2018-10-25 22:13:48 +0300

    Update ThingService.java

--- src/main/java/com/shilko/ru/witcher/service/ThingService.java
+++ src/main/java/com/shilko/ru/witcher/service/ThingService.java
@@ -6,13 +6,18 @@ public interface ThingService {
   void addNewThing(Thing newThing);
   
   //edditing:
-  void editThing(Long whichThing, Thing thisThing);
+  void editThing(Long thingID);
+  
+  void editThing(String thingName);
+  
+  void removeThing(Long thingID);
+  
+  void removeThing(String thingName);
   
   //getters:
   Thing getThingById(Long id);
   Thign getThingByName(String thingName);
-  //some list or map here:
-  //<Thing> getAllThings();
   
   //what kind is this thing???
+  
 }
