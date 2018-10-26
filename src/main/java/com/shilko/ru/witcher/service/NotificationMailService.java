766c355f918f5cac796cf592ff6099d6f819b3f8
ChaosBah <37620958+ChaosBah@users.noreply.github.com>
2018-10-26 12:25:47 +0300

    Update NotificationMailService.java

--- src/main/java/com/shilko/ru/witcher/service/NotificationMailService.java
+++ src/main/java/com/shilko/ru/witcher/service/NotificationMailService.java
@@ -2,13 +2,8 @@ package com.shilko.ru.witcher.service;
 
 public interface NotificationMailService {
   
-  //String message;
-  //String email;
+  void mailto(String email, String message);
   
-  //void setMessage(String message);
-  
-  //void setEmail(String email);
-  
-  void mailto(String message, String email);
+  void mailto(String email, String theme, String message);
      
 }
