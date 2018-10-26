457129b3d428ec8f1144881345c16d2a747bf895
ChaosBah <37620958+ChaosBah@users.noreply.github.com>
2018-10-26 12:52:11 +0300

    Update DraftService.java

--- src/main/java/com/shilko/ru/witcher/service/DraftService.java
+++ src/main/java/com/shilko/ru/witcher/service/DraftService.java
@@ -1,23 +1,4 @@
 package com.shilko.ru.witcher.service;
 
 public interface DraftService {
-  
-  //check on draft:
-  boolean isComponentByDraft(Component component);
-  
-  boolean isThingByDraft(Thing thing);
-  
-  boolean isComponentEdited(Component component);
-  
-  boolean isThingEdited(Thing thing);
-  
-  //check on whether this draft was carried out earlier with this thing and component:
-  boolean isDrafted(Component component, Thing thing);
-  
-  void draft(Component whichComponent, Thing whichThing);
-  
-  void getResult();
-  
-  void saveDraftResult();
-  
 }
