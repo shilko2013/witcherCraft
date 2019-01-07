5b5609aaeadae7d2217c2b5e8e1b81536d51fd73
shilko2013 <shilko2013@mail.ru>
2019-01-07 15:28:19 +0300

    Version 0.9.8.2. External resources open for anonymous.

--- src/main/java/com/shilko/ru/witcher/controller/api/ExternalResourceController.java
+++ src/main/java/com/shilko/ru/witcher/controller/api/ExternalResourceController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/resource")
+@RequestMapping("/resource")
 public class ExternalResourceController {
 
     @Autowired
